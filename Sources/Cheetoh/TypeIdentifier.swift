a464f6ee1122f7ab9faeffee81ffb4539bea56f3
k-ymmt <account@kymmt.net>
2020-02-05 19:02:18 +0900

    Add: Type Identifier

---
+++ Sources/Cheetoh/TypeIdentifier.swift
@@ -0,0 +1,37 @@
+//
+//  TypeIdentifier.swift
+//  Cheetoh
+//
+//  Created by Kazuki Yamamoto on 2020/01/31.
+//
+
+import Foundation
+import SwiftSyntax
+
+public struct TypeIdentifier: SyntaxBuildable, GenericArgumentsProtocol, NillableType {
+    public private(set) var syntax: SyntaxValues = SyntaxValues()
+    
+    let name: String
+    
+    public init(_ name: String) {
+        self.name = name
+    }
+    
+    public func environment<V>(_ keyPath: WritableKeyPath<SyntaxValues, V>, _ value: V) -> TypeIdentifier {
+        var identifier = self
+        identifier.syntax[keyPath: keyPath] = value
+        return identifier
+    }
+    
+    public func build(format: Format) -> TypeSyntax {
+        let type = SimpleTypeIdentifierSyntax {
+            $0.useName(SyntaxFactory.makeIdentifier(name))
+
+            if let generics = buildGenericArguments(format: format) {
+                $0.useGenericArgumentClause(generics)
+            }
+        }
+        
+        return buildNillableType(type: type)
+    }
+}
