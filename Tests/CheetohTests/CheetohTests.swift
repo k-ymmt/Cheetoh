2b326b6d6b2e6a1fe477aa2c67b7ba06b9f90d5e
k-ymmt <account@kymmt.net>
2020-02-05 19:07:36 +0900

    Update: Test

--- Tests/CheetohTests/CheetohTests.swift
+++ Tests/CheetohTests/CheetohTests.swift
@@ -3,6 +3,23 @@ import XCTest
 
 final class CheetohTests: XCTestCase {
     func testExample() {
+        let example = Struct("Foo") {
+            Let(name: "hoge", type: TypeIdentifier("Hoge"))
+            Var(
+                name: "bar",
+                type: TypeIdentifier("Bar")
+                    .nillable()
+                    .genericArguments(TypeIdentifier("Foo"))
+            ).accessLevel(.private)
+        }.build(format: .init(indent: 4))
+        
+        XCTAssertEqual(example.description, """
+struct Foo {
+    let hoge: Hoge
+    private var bar: Bar<Foo>?
+}
+
+""")
     }
 
     static var allTests = [
