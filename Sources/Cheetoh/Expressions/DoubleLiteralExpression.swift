6b75595a9424953da891eb3a382155fb9e581b97
k-ymmt <account@kymmt.net>
2020-02-09 00:51:28 +0900

    Add: DoubleLiteral

---
+++ Sources/Cheetoh/Expressions/DoubleLiteralExpression.swift
@@ -0,0 +1,34 @@
+//
+//  DoubleLiteralExpression.swift
+//  Cheetoh
+//
+//  Created by Kazuki Yamamoto on 2020/02/07.
+//
+
+import Foundation
+import SwiftSyntax
+
+public struct FloatLiteralExpression: SyntaxBuildable, Expression {
+    public private(set) var syntax: SyntaxValues = SyntaxValues()
+    
+    private let text: String
+    
+    public init(_ text: String) {
+        self.text = text
+    }
+
+    public func environment<V>(_ keyPath: WritableKeyPath<SyntaxValues, V>, _ value: V) -> Self {
+        var me = self
+        me.syntax[keyPath: keyPath] = value
+        
+        return me
+    }
+    
+    public func build(format: Format) -> FloatLiteralExprSyntax {
+        SyntaxFactory.makeFloatLiteralExpr(floatingDigits: SyntaxFactory.makeIdentifier(text))
+    }
+    
+    public func buildExpression(format: Format) -> ExprSyntax {
+        build(format: format)
+    }
+}
