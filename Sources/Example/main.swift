9035aac4c7fef46ae49a2f070144f6abfa74cdac
k-ymmt <account@kymmt.net>
2020-02-09 01:01:27 +0900

    Update: Example

--- Sources/Example/main.swift
+++ Sources/Example/main.swift
@@ -3,14 +3,27 @@ import Cheetoh
 
 var output: String = ""
 
-Struct("Foo") {
-    Let(name: "hoge", type: TypeIdentifier("Hoge"))
-        .initializer(StringLiteralExpression("hoge"))
-    Var(name: "bar", type: TypeIdentifier("Bar")
-        .nillable()
-        .genericArguments(TypeIdentifier("Foo"))
-    ).accessLevel(.private)
-    .initializer(StringLiteralExpression("bar"))
-}.build(format: Format(indent: 4)).write(to: &output)
+SourceFile {
+    Import("Foundation")
+    
+    Newlines()
+    
+    Struct("Hoge") {
+        Let("hoge", String.self)
+            .initializer(StringLiteralExpression("hoge"))
+        Var("foo", Int.self)
+    }
+    
+    Newlines()
+    
+    Let("hoge", TypeIdentifier("Hoge"))
+        .initializer(FunctionCallExpression("Hoge"))
+
+    Newlines()
+
+    Func("main") {
+        FunctionCallExpression("print", ["": StringLiteralExpression("Hello World!")])
+    }
+}.build(format: .init(indent: 4)).write(to: &output)
 
 print(output)
