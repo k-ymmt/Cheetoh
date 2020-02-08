3c0711c77aa62f71a3708a34aa2e0b23654b49b4
k-ymmt <account@kymmt.net>
2020-02-09 00:55:33 +0900

    Add: SourceFile

---
+++ Sources/Cheetoh/Declarations/SourceFile.swift
@@ -0,0 +1,47 @@
+//
+//  SourceFile.swift
+//  Cheetoh
+//
+//  Created by Kazuki Yamamoto on 2020/02/08.
+//
+
+import Foundation
+import SwiftSyntax
+
+public protocol SourceFileScopeDeclaration: DeclMemberProtocol {
+}
+
+@_functionBuilder
+public struct SourceFileScopeDeclarationListBuilder {
+    public static func buildBlock(_ list: SourceFileScopeDeclaration...) -> [SourceFileScopeDeclaration] {
+        list
+    }
+}
+
+public struct SourceFile: SyntaxBuildable {
+    public var syntax: SyntaxValues = SyntaxValues()
+    
+    private let list: [SourceFileScopeDeclaration]
+    
+    public init(@SourceFileScopeDeclarationListBuilder builder: () -> [SourceFileScopeDeclaration]) {
+        self.list = builder()
+    }
+    
+    public func environment<V>(_ keyPath: WritableKeyPath<SyntaxValues, V>, _ value: V) -> SourceFile {
+        var me = self
+        me.syntax[keyPath: keyPath] = value
+
+        return me
+    }
+    
+    public func build(format: Format) -> SourceFileSyntax {
+        SourceFileSyntax {
+            for item in list {
+                $0.addStatement(CodeBlockItemSyntax {
+                    $0.useItem(item.buildDeclMember(format: format))
+                }.withTrailingTrivia(.newlines(1)))
+            }
+            $0.useEOFToken(SyntaxFactory.makeUnknown("").withLeadingTrivia(.newlines(1)))
+        }
+    }
+}
