615268c6cffa67a5a87b4984f261f6df6d5b5631
k-ymmt <account@kymmt.net>
2020-02-18 19:32:26 +0900

    Add: Nil Literal

---
+++ Sources/Cheetoh/Expressions/NilLiteral.swift
@@ -0,0 +1,31 @@
+//
+//  NilLiteral.swift
+//  Cheetoh
+//
+//  Created by Kazuki Yamamoto on 2020/02/11.
+//
+
+import Foundation
+import SwiftSyntax
+
+public struct NilLiteral: SyntaxBuildable, Expression {
+    public private(set) var syntax: SyntaxValues = SyntaxValues()
+    
+    public init() {
+    }
+
+    public func environment<V>(_ keyPath: WritableKeyPath<SyntaxValues, V>, _ value: V) -> Self {
+        var me = self
+        me.syntax[keyPath: keyPath] = value
+        
+        return me
+    }
+    
+    public func build(format: Format) -> NilLiteralExprSyntax {
+        SyntaxFactory.makeNilLiteralExpr(nilKeyword: SyntaxFactory.makeNilKeyword())
+    }
+    
+    public func buildExpression(format: Format) -> ExprSyntax {
+        build(format: format)
+    }
+}
