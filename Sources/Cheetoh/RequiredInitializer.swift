4ecf21d52b91ec7fd68b29307280c97d0a906ac8
k-ymmt <account@kymmt.net>
2020-04-01 21:37:19 +0900

    Add: required

---
+++ Sources/Cheetoh/RequiredInitializer.swift
@@ -0,0 +1,37 @@
+//
+//  RequiredInitializer.swift
+//  Cheetoh
+//
+//  Created by Kazuki Yamamoto on 2020/03/30.
+//
+
+import Foundation
+import SwiftSyntax
+
+extension SyntaxValues {
+    var required: Bool? {
+        get {
+            values[\Self.required] as? Bool
+        }
+        set {
+            values[\Self.required] = newValue
+        }
+    }
+}
+
+public protocol RequiredInitializerProtocol: SyntaxBuildable {
+    func require() -> SelfType
+}
+
+extension RequiredInitializerProtocol {
+    public func required() -> SelfType {
+        return environment(\.required, true)
+    }
+    
+    func buildRequired() -> TokenSyntax? {
+        guard let required = syntax.required, required else {
+            return nil
+        }
+        return SyntaxFactory.makeUnknown("required", trailingTrivia: .spaces(1))
+    }
+}
