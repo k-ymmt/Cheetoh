899871c81bc28cf32bd57e7f0aa5345b47c720d9
k-ymmt <account@kymmt.net>
2020-04-01 21:36:19 +0900

    Add: Assign

---
+++ Sources/Cheetoh/Expressions/Assign.swift
@@ -0,0 +1,49 @@
+//
+//  Assign.swift
+//  Cheetoh
+//
+//  Created by Kazuki Yamamoto on 2020/04/01.
+//
+
+import Foundation
+import SwiftSyntax
+
+public struct Assign: SyntaxBuildable {
+    public private(set) var syntax: SyntaxValues = SyntaxValues()
+    
+    let left: Expression
+    let right: Expression
+    
+    public init(_ left: Expression, _ right: Expression) {
+        self.left = left
+        self.right = right
+    }
+    
+    public func environment<V>(_ keyPath: WritableKeyPath<SyntaxValues, V>, _ value: V) -> Self {
+        var me = self
+        me.syntax[keyPath: keyPath] = value
+        
+        return me
+    }
+    
+    public func build(format: Format) -> SequenceExprSyntax {
+        SyntaxFactory.makeSequenceExpr(elements: SyntaxFactory.makeExprList([
+            left.buildExpression(format: format),
+            SyntaxFactory.makeAssignmentExpr(assignToken: SyntaxFactory.makeUnknown(
+                "=",
+                leadingTrivia: .spaces(1),
+                trailingTrivia: .spaces(1)
+            )),
+            right.buildExpression(format: format)
+        ]))
+    }
+}
+
+extension Assign: CodeBlockItem {
+    public func buildCodeBlockItem(format: Format) -> CodeBlockItemSyntax {
+        CodeBlockItemSyntax {
+            $0.useItem(build(format: format))
+        }
+    }
+}
+
--- Sources/Cheetoh/Expressions/Expression.swift
+++ Sources/Cheetoh/Expressions/Expression.swift
@@ -8,45 +8,6 @@
 import Foundation
 import SwiftSyntax
 
-struct Substitution: SyntaxBuildable {
-    public private(set) var syntax: SyntaxValues = SyntaxValues()
-    
-    let identifierName: String
-    let expression: Expression
-    
-    public init(_ targetName: String, _ expression: Expression) {
-        self.identifierName = targetName
-        self.expression = expression
-    }
-    
-    public func environment<V>(_ keyPath: WritableKeyPath<SyntaxValues, V>, _ value: V) -> Self {
-        var me = self
-        me.syntax[keyPath: keyPath] = value
-        
-        return me
-    }
-    
-    func build(format: Format) -> ExprListSyntax {
-        SyntaxFactory.makeExprList([
-            IdentifierExprSyntax {
-                $0.useIdentifier(SyntaxFactory.makeIdentifier(identifierName))
-            },
-            BinaryOperatorExprSyntax {
-                $0.useOperatorToken(SyntaxFactory.makeOperatorKeyword(leadingTrivia: .spaces(1), trailingTrivia: .spaces(1)))
-            },
-            expression.buildExpression(format: format)
-        ])
-    }
-}
-
-extension Substitution: CodeBlockItem {
-    func buildCodeBlockItem(format: Format) -> CodeBlockItemSyntax {
-        CodeBlockItemSyntax {
-            $0.useItem(build(format: format))
-        }
-    }
-}
-
 public protocol Expression: CodeBlockItem {
     func buildExpression(format: Format) -> ExprSyntax
 }
