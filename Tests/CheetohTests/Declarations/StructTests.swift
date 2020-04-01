afd465a51ab64a91e36ebe03e5012be1db8676b7
k-ymmt <account@kymmt.net>
2020-04-01 21:39:21 +0900

    Add: Struct Test

---
+++ Tests/CheetohTests/Declarations/StructTests.swift
@@ -0,0 +1,47 @@
+//
+//  StructTests.swift
+//  Cheetoh
+//
+//  Created by Kazuki Yamamoto on 2020/03/29.
+//
+
+import Foundation
+import XCTest
+@testable import Cheetoh
+
+private let structText = """
+public struct Foo: CustomStringConvertible {
+    private let string: String
+
+    init(string: String) {
+        self.string = string
+    }
+
+    func description() -> String {
+        return string
+    }
+}
+
+"""
+
+class StructTests: XCTestCase {
+    func testStruct() {
+        let s = Struct("Foo") {
+            Let("string", String.self)
+                .accessLevel(.private)
+            Newlines()
+            
+            Init(ParameterVariable("string", type: String.self)) {
+                Assign(Identifier("self").member("string"), Identifier("string"))
+            }
+            Newlines()
+            
+            Func("description") {
+                Return(Identifier("string"))
+            }.returnType(TypeIdentifier("String"))
+        }.accessLevel(.public)
+        .inheritedTypes(TypeIdentifier("CustomStringConvertible"))
+        
+        XCTAssertEqual(s.build(format: Format(indent: 4)).description, structText)
+    }
+}
