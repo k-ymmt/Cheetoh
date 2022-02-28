cb476efa332ef184ac5a28d1a8cfeb54e5406595
k-ymmt <account@kymmt.net>
2022-02-28 19:53:04 +0900

    update swift toolchain & fix compile error

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -17,14 +17,16 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.50400.0"))
+        .package(url: "https://github.com/apple/swift-syntax", branch: "0.50600.0-SNAPSHOT-2022-01-24")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Cheetoh",
-            dependencies: ["SwiftSyntax"]),
+            dependencies: [
+                .product(name: "SwiftSyntax", package: "swift-syntax")
+            ]),
         .testTarget(
             name: "CheetohTests",
             dependencies: ["Cheetoh"]),
